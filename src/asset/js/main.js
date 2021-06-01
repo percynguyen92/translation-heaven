da9ff35e3183ccbb58b40cafd3f8e0df37c8d531
percynguyen92 <percynguyen92@gmail.com>
2021-06-01 22:51:43 +0700

    trash

--- src/asset/js/main.js
+++ src/asset/js/main.js
@@ -4,22 +4,22 @@ const $ = require('jquery');
 
 $('#closeBtn').on('click', function () {
     ipc.send('closeApp');
-})
+});
 
 $('#maximizeBtn').on('click', function () {
     ipc.send('maximizeApp');
     console.log("max");
-})
+});
 
 $('#restoreBtn').on('click', function () {
     ipc.send('restoreApp');
     console.log("max");
-})
+});
 
 $('#minimizeBtn').on('click', function () {
     ipc.send('minimizeApp');
-})
+});
 
 ipc.on('maximized', (event, message) => {
     console.log(message);
-})
+});
